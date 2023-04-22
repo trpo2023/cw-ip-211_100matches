db657d858c518ccfebefd85c9225c0f362d1f18e
Artem <115490435+SisAdminNsk@users.noreply.github.com>
2023-04-22 14:03:44 +0700

    Update play.cpp

--- src/LibOfProject/play.cpp
+++ src/LibOfProject/play.cpp
@@ -9,22 +9,20 @@ void play(const std::vector<int>& settings)
     int decMatches = 0;
     while (isGame) {
         system("clear");
-        std::cout << "Спичек осталось: " << matchesRemaining << "\n";
+        std::cout << "Г‘ГЇГЁГ·ГҐГЄ Г®Г±ГІГ Г«Г®Г±Гј: " << matchesRemaining << "\n";
         if (first.isMove)
-            std::cout << "Сейчас ходит:" << first._name << " \n";
+            std::cout << "Г‘ГҐГ©Г·Г Г± ГµГ®Г¤ГЁГІ:" << first._name << " \n";
         else
-            std::cout << "Сейчас ходит:" << second._name << " \n";
-        std::cout << "Введите желаемое кол-во спичек [" << 1 << "-"
+            std::cout << "Г‘ГҐГ©Г·Г Г± ГµГ®Г¤ГЁГІ:" << second._name << " \n";
+        std::cout << "Г‚ГўГҐГ¤ГЁГІГҐ Г¦ГҐГ«Г ГҐГ¬Г®ГҐ ГЄГ®Г«-ГўГ® Г±ГЇГЁГ·ГҐГЄ [" << 1 << "-"
                   << settings[1] << "]\n";
         do {
             std::cin >> decMatches;
-            if (TestDec(settings, decMatches)) // проверка попадения в диапазон
-                                               // [1 до settings[0]]
+            if (TestDec(settings, decMatches))
             {
                 if (takeAway(
                             matchesRemaining,
-                            decMatches)) // проверка можно ли отнять это кол-во
-                                         // от matchesRemaining
+                            decMatches))
                 {
                     matchesRemaining -= decMatches;
                     if (first.isMove) {
@@ -36,16 +34,16 @@ void play(const std::vector<int>& settings)
                     }
                     break;
                 } else
-                    std::cout << "У вас нет " << decMatches << " спичек!\n";
+                    std::cout << "Г“ ГўГ Г± Г­ГҐГІ " << decMatches << " Г±ГЇГЁГ·ГҐГЄ!\n";
             } else
-                std::cout << "Неверный ввод!\n";
+                std::cout << "ГЌГҐГўГҐГ°Г­Г»Г© ГўГўГ®Г¤!\n";
         } while (true);
         if (matchesRemaining == 0) {
             system("clear");
             if (first.isMove)
-                std::cout << "Игрок " << second._name << " победил!";
+                std::cout << "Г€ГЈГ°Г®ГЄ " << second._name << " ГЇГ®ГЎГҐГ¤ГЁГ«!";
             else
-                std::cout << "Игрок " << first._name << " победил!";
+                std::cout << "Г€ГЈГ°Г®ГЄ " << first._name << " ГЇГ®ГЎГҐГ¤ГЁГ«!";
             isGame = false;
         }
     }
@@ -53,9 +51,9 @@ void play(const std::vector<int>& settings)
 bool takeAway(int& matchesCount, int dec) // assert
 {
     if (matchesCount >= dec)
-        return true;  // OK
+        return true;
     else
-        return false; // No ok
+        return false;
 }
 bool TestDec(const std::vector<int>& settings, int dec)
 {
