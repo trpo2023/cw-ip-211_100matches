29da1f037a0d0c1971e57067273d83376d73410a
SisAdminNsk <artemoktan777@gmail.com>
2023-04-26 14:54:24 +0700

    Added new group of gameLogic Test

--- test_src/test_lib/test.cpp
+++ test_src/test_lib/test.cpp
@@ -1,9 +1,10 @@
 #include <ctest.h>
 #include <play.h>
+#include <vector>
 
 // Given
 
-CTEST(BOOL_suite, exceeding_the_limit)
+CTEST(GameLogicTest_Suite, unexceeding_the_limit)
 {
     const int dec = 1;
     const int matchesCount = 100;
@@ -12,5 +13,40 @@ CTEST(BOOL_suite, exceeding_the_limit)
 
     // Then
     const bool expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(GameLogicTest_Suite,exceeding_the_limit)
+{
+    const int dec = 10;
+    const int matchesCount = 1;
+    // When
+    const bool result = takeAway(matchesCount, dec);
+
+    // Then
+    const bool expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(GameLogicTest_Suite, unexceeding_settings_limit)
+{
+    const int dec = 5;
+    std::vector<int> settings = {100,10};
+    // When
+    const bool result = TestDec(settings, dec);
+    // Then
+    const bool expected = 1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(GameLogicTest_Suite, exceeding_settings_limit)
+{
+    const int dec = 20;
+    std::vector<int> settings = { 100,10 };
+    // When
+    const bool result = TestDec(settings, dec);
+
+    // Then
+    const bool expected = 0;
     ASSERT_EQUAL(expected, result);
 }
