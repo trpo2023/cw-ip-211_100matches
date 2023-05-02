3f1a61cafc66ae074840b09cbbe510110846a231
SisAdminNsk <artemoktan777@gmail.com>
2023-05-02 23:42:21 +0700

    add headers, to do unit tests

--- src/LibOfProject/settings.h
+++ src/LibOfProject/settings.h
@@ -1,4 +1,22 @@
 #pragma once
 #include <vector>
+#include <string>
 
 void settings(std::vector<int>& defaultSettings);
+void FirstSettingsMessage(unsigned int currentSettings);
+void SecondSettingsMessage(unsigned int currentSettings);
+
+int CheckInput(std::string userInput);
+void ResetSettings(std::vector<int>& defaultSettings);
+
+class InputHandler
+{
+public:
+    bool ChangeFirstSettings(std::vector<int>& defaultSettings, unsigned int userInput);
+    bool ChangeSecondSettings(std::vector<int>& defaultSettings, unsigned int userInput);
+};
+
+
+
+
+
